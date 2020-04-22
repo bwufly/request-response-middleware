77d08b42fb366bd19ae4e549f1c5a425c8ea0739
wufly <wfxykzd@163.com>
2020-04-22 17:09:51 +0800

    update

--- src/Storage/migrations/2020_04_22_100000_create_request_log_entries_table.php
+++ src/Storage/migrations/2020_04_22_100000_create_request_log_entries_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTelescopeEntriesTable extends Migration
+class CreateRequestLogEntriesTable extends Migration
 {
     /**
      * The database schema.
